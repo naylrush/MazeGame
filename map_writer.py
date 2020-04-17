3cd493f4b3b42163b9a2f77ea67e7ecdc672dc2a
Alexey Davletshin <naylrush@gmail.com>
2020-04-18 00:54:24 +0300

    Fix and upgrade map_writer

--- map_writer.py
+++ map_writer.py
@@ -10,38 +10,43 @@ def write_map(map, path=''):
 
     empty_sym = Empty().to_symbol()
     empty_border_sym = ' '
-    right_border_sym = RIGHT.to_symbol()
-    down_border_sym = DOWN.to_symbol()
 
     unique_cells = {}
 
     # generate symbol map
+    teleport_count = 0
     for i in range(map.x_size * 2 - 1):
         for j in range(map.y_size * 2 - 1):
             cell = map[Position(i // 2, j // 2)]
             if i & 1 == 0:
                 if j & 1 == 0:
-                    sym_map[i].append(cell.to_symbol())
-                    unique_cells[cell.to_symbol()] = deepcopy(cell)
+                    cell_symbol = cell.to_symbol()
+                    if type(cell) is Teleport:
+                        teleport_count += 1
+                        cell_symbol = str(teleport_count)
+                    sym_map[i].append(cell_symbol)
+                    unique_cells[cell_symbol] = deepcopy(cell)
                 else:
-                    sym_map[i].append(right_border_sym if cell.has_border_at(RIGHT) else empty_border_sym)
+                    sym_map[i].append(RIGHT.to_symbol() if cell.has_border_at(RIGHT) else empty_border_sym)
             else:
                 if j & 1 == 0:
-                    sym_map[i].append(down_border_sym if cell.has_border_at(DOWN) else empty_sym)
+                    sym_map[i].append(DOWN.to_symbol() if cell.has_border_at(DOWN) else empty_sym)
                 else:
                     sym_map[i].append(empty_border_sym)
 
     unique_cells.pop(Empty().to_symbol())
     # generate command by symbol
     symbol_command = {}
-    for cell in unique_cells.values():
+    for cell_symbol, cell in unique_cells.items():
         command = cell.name + '('
-        if cell is Exit or cell is RubberRoom:
+        if type(cell) is Exit or type(cell) is RubberRoom:
             command += cell.direction.name
-        if cell is Stun:
+        elif type(cell) is Stun:
             command += str(cell.duration)
+        elif type(cell) is Teleport:
+            command += str(cell.destination)
         command += ')'
-        symbol_command[cell.to_symbol()] = command
+        symbol_command[cell_symbol] = command
 
     # print
     if path == '':
